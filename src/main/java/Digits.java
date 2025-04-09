6c44417de8c8b8f701270698e897e6520573cd9f
cybercrow <malcolmbarrykao@gmail.com>
2025-04-09 12:21:29 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -6,13 +6,23 @@ public class Digits
 	private ArrayList<Integer> digitList;
 
 	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    
+	{ 
+	  ArrayList <Integer> digitList = new ArrayList <Integer>();  
+    if(len==0)
+      digitList.add(0);
+    for (int i=0;i>digitList.size();i++)
+      digitList.add(len/10,len%10);
+    return digitList;   
 	}
 
 	public boolean isStrictlyIncreasing()
-	{ /* to be implemented in part (b) */
-		
+	{ 
+	 int lastvalue=0;
+   for(int i=0; i < digitList.size(); i++){
+     if(digitList.get(i)<=lastvalue)
+       return false; 
+    if(digitList.get(i))> lastvalue)
+      lastvalue=digitList.get(i);	
 
 	}
 	
